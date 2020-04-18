0b6ec12609901be871e15dafa29277fd830452ae
Bennett <bennybenester@gmail.com>
2020-04-18 12:25:32 +0300

    :sparkles: add base configuration file

---
+++ backend/src/core/base.configurations.ts
@@ -0,0 +1,36 @@
+import * as fs from 'fs';
+let pathFolder = process.env.HRIS_HOME;
+if (!pathFolder) {
+  if (!fs.existsSync('./files')) {
+    fs.mkdirSync('./files');
+  }
+  if (!fs.existsSync('./files/config.json')) {
+    fs.writeFileSync(
+      './files/config.json',
+      fs.readFileSync('./config.example.json'),
+    );
+  }
+  pathFolder = __dirname.split('/src/database/config').join('') + '/files';
+}
+const config = JSON.parse(
+  fs.readFileSync(pathFolder + '/' + 'config.json', 'utf8'),
+);
+
+export function getDataBaseConfiguration() {
+  return {
+    ...config.database,
+    entities: ['src/**/*.entity{.ts,.js}'],
+    migrations: ['src/database/migration/*.ts'],
+  };
+}
+export function getConfiguration() {
+  const files = config.files || {};
+  if (!config.port) {
+    config.port = 3000;
+  }
+
+  return {
+    ...config,
+    ...files,
+  };
+}
