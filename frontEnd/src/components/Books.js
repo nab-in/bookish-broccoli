3b428dc37553b5590af15a3ccb1653c8c097490f
Rafii Ramii <info.rafyramy@gmail.com>
2020-04-24 14:25:01 +0300

    added books components and call them

--- frontEnd/src/components/Books.js
+++ frontEnd/src/components/Books.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import book1 from '../images/alchemist.jpg'
+// import book1 from '../images/alchemist.jpg'
 import books from '../styles/books.module.css'
-const Books = ()=> {
-    return (
-        <div className={books.container}>
- <div className={books.contents}>
-            <img className={books.bookImage} src={book1} alt="alchemist"/>
 
-            <div className={books.about}>
-                <h4 className={books.title}>
-                    Alchemist vol2
-                </h4>
-                <p className={books.author}>
-                    by Paul coleho
-                </p>
-            </div>
+const Books = props => 
+    
+    <div className={books.container}>
+<div className={books.contents}>
+        <img className={books.bookImage} src={props.bookUrl} alt={props.bookName}/>
+
+        <div className={books.about}>
+            <h4 className={books.title}>
+                {props.bookName}
+            </h4>
+            <p className={books.author}>
+                {props.bookSummary}
+            </p>
         </div>
-        </div>
-       
-    )
-}
+    </div>
+    </div>
+   
+
 export default Books
--- frontEnd/src/components/Recommended.js
+++ frontEnd/src/components/Recommended.js
@@ -1,21 +1,40 @@
 import React from 'react'
 import recomends from '../styles/recommended.module.css'
+// import books from '../styles/books.module.css'
+
 import Books from '../components/Books'
-import book1 from '../images/5amClub.png'
-import book2 from '../images/11minutes.jpg'
+import book3 from '../images/11minutes.jpg'
+import book1 from '../images/here for it.jpg'
+import book2 from '../images/the resisters.jpg'
+import book4 from '../images/poweNotebook.jpg'
+import book5 from '../images/glass.jpg'
 
-// const Books = ()=><div>
-// {props.img}
-// </div> 
+
+const bookData =[
+    {bookUrl:[book1], bookName:'here for it', bookSummary:'by rafyramy'},
+    {bookUrl:[book2], bookName:'the resisters', bookSummary:'by paul'},
+    {bookUrl:[book3], bookName:'fiveAmClub', bookSummary:'by rafyramy'},
+    {bookUrl:[book4], bookName:'power notebook', bookSummary:'by paul'},
+    {bookUrl:[book5], bookName:'the glass hotel', bookSummary:'by rafyramy'},
+   
+          
+];
+
 const Recommends = () => {
-    return(
-        <div className={recomends.container}>
-             <h2 className={recomends.title}>Recommended For You</h2>
+    
+        return(
+            <div className={recomends.container}>
+                 <h2 className={recomends.title}>Recommended For You</h2>
+    
+                 <div className={recomends.bookContents}>
+                     {bookData.map(info =>
+                     <Books{...info}/>
+                     )}
+                 </div>
+                
+            </div>
+        );
+    }
+ 
 
-             <Books/>
-            
-             {/* <Books img={book2}/> */}
-        </div>
-    )
-}
 export default Recommends
