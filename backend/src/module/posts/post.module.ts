42cb88a99c43177da6f0d22e8ef13e95a7187cca
Bennett <bennybenester@gmail.com>
2020-04-26 20:50:27 +0300

    :fire: rename post to posts

--- backend/src/module/posts/post.module.ts
+++ backend/src/module/posts/post.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { PostController } from './controllers/post.controller';
 import { PostService } from './services/post.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Post } from './entities/post.entity';
+import { Posts } from './entities/post.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([Posts])],
   controllers: [PostController],
   providers: [PostService],
 })
